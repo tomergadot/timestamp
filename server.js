fb68829010cfcf811f66986fff6f8ea73c623d17
Tomer Gadot <tomergadot@gmail.com>
2016-10-15 22:48:04 +0000

    logging

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/new/:url(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}\/?)', function (req, res) {
+    console.log("Got request with site: " + req.params.url);
     var site_id = generateSiteId();
     updateSiteId(site_id, req.params.url);
     res.end(site_id.toString());
-
 });
 
 app.get('/:id', function (req, res) {
@@ -35,10 +35,11 @@ function generateSiteId() {
 function updateSiteId(id, site) {
     mongo.connect(URL, function(err, db) {
       // db gives access to the database
-      if(err) return err;
+      if(err) return console.log(err);
+      console.log("updateSiteId: Connected to db");
       var collection = db.collection('sites');
       collection.update({id: id}, {$set:{site:site}}, {upsert:true} , function(err, data) {
-          if(err) return err;
+          if(err) return console.log(err);
           console.log("written: " + JSON.stringify(data));});
       db.close();
 });
@@ -47,10 +48,11 @@ function updateSiteId(id, site) {
 function redirectToSite(id, res) {
     mongo.connect(URL, function(err, db) {
       // db gives access to the database
-      if(err) return err;
+      if(err) return console.log(err);
+      console.log("redirectToSite: Connected to db");
       var collection = db.collection('sites');
       collection.find({id: 1}).toArray(function(err, documents) {
-          if(err) return err;
+          if(err) return console.log(err);
           console.log("read: " + documents[0].site);
           res.redirect(documents[0].site);
           });
